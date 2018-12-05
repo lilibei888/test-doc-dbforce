a8d7644dc89bf1d05abf8b6eb1bc20805b07f801
Nidhi <lili.bei@outlook.com>
2018-12-05 14:46:22 +0100

    Resolve service definition error

--- Force/DBDoc/Controller/DocumentorController.php
+++ Force/DBDoc/Controller/DocumentorController.php
@@ -1,7 +1,14 @@
 <?php
 namespace Force\DBDoc\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
+
 class DocumentorController
 {
+    public function allAction()
+    {
+        return new Response('Hello world');
+    }
 }
 
